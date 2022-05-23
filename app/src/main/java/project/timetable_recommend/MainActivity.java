d5516e6533011a7d9cab780017b9915ff723acd5
Kim Seok Hyun <sam0774@naver.com>
2022-05-23 19:22:33 +0900

    complete MBTIQuestion

--- app/src/main/java/project/timetable_recommend/MainActivity.java
+++ app/src/main/java/project/timetable_recommend/MainActivity.java
@@ -2,7 +2,9 @@ package project.timetable_recommend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String MBTI = intent.getStringExtra("MBTI");
+        Toast.makeText(getApplicationContext(), "당신의 MBTI는 " + MBTI + "입니다.", Toast.LENGTH_SHORT)
+                .show();
 
-
-
-        
     }
 }
--- app/src/main/java/project/timetable_recommend/SettingActivity.java
+++ app/src/main/java/project/timetable_recommend/SettingActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -14,25 +18,43 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        TextView textView = findViewById(R.id.textView);
+        EditText editText = findViewById(R.id.MBTI);
+        Button button = findViewById(R.id.button2);
 
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
         dialogBuilder.setTitle("MBTI 검사 하시겠습니까?").setMessage("");
+        //OK클릭 시
         dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id)
             {
-                Toast.makeText(getApplicationContext(), "MBTI 검사를 시작하겠습니다.", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(getApplicationContext(), MBTIQuestionActivity.class);
+                startActivity(intent);
             }
         });
-
+        //cancel 클릭 시
         dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
             @Override
-            public void onClick(DialogInterface dialog, int id)
-            {
-                Toast.makeText(getApplicationContext(), "자신의 MBTI 유형을 입력해주세요", Toast.LENGTH_SHORT).show();
+            public void onClick(DialogInterface dialog, int id) {
+                textView.setVisibility(View.VISIBLE);
+                editText.setVisibility(View.VISIBLE);
+                button.setVisibility(View.VISIBLE);
             }
         });
         dialogBuilder.show();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String MBTI = editText.getText().toString();
+                if(Character.isLowerCase(MBTI.charAt(0)))
+                    MBTI = MBTI.toUpperCase();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                intent.putExtra("MBTI", MBTI);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
