367563c747d63fee0a40fdc8cadf132bc6a575bb
ysh <happysh_s2@naver.com>
2022-05-25 01:38:50 +0900

    Fix recycler view

--- app/src/main/java/Model/MbtiRecyclerViewCell.java
+++ app/src/main/java/Model/MbtiRecyclerViewCell.java
@@ -1,40 +1,24 @@
 package Model;
 
-import android.view.View;
 import android.widget.TextView;
-
-import androidx.annotation.NonNull;
-
-
 
 import project.timetable_recommend.R;
 
-public class MbtiListCell {
-    private TextView mbtiType , mbtiInfo;
-    public enum textType{mbti_info, mbti_type};
-    public MbtiListCell(){ }
+public class MbtiRecyclerViewCell {
 
     public final int textID[] = {R.id.mbti_info, R.id.mbti_type};
 
-    public void setTextViewID(@NonNull View itemView){
-        mbtiInfo = (TextView) itemView.findViewById(textID[textType.mbti_info.ordinal()]);
-        mbtiType = (TextView) itemView.findViewById(textID[textType.mbti_type.ordinal()]);
+    public TextView mbtiType , mbtiInfo;
+
+    public int mbti_info_num ;
+    public int mbti_type_num ;
+
+    public MbtiRecyclerViewCell(){
+        mbti_info_num = 0;
+        mbti_type_num = 1;
     }
 
 
-    public TextView getMbtiType() {
-        return mbtiType;
-    }
 
-    public void setMbtiType(TextView mbtiType) {
-        this.mbtiType = mbtiType;
-    }
-
-    public TextView getMbtiInfo() {
-        return mbtiInfo;
-    }
-
-    public void setMbtiInfo(TextView mbtiInfo) {
-        this.mbtiInfo = mbtiInfo;
-    }
 }
+
--- app/src/main/java/project/timetable_recommend/MBTIActivity.java
+++ app/src/main/java/project/timetable_recommend/MBTIActivity.java
@@ -12,27 +12,30 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import Model.MBTIData;
 import Model.MbtiType;
 
 public class MBTIActivity extends AppCompatActivity {
-    BottomNavigationView bottomNavigationView;
-    LinearLayoutManager layoutManager;
-    RecyclerView               recycleView;
+    BottomNavigationView       bottomNavigationView;
+    RecyclerView.LayoutManager layoutManager;
     MBTIAdapter                adapter;
-
+    RecyclerView               recycleView;
+    MBTIData                   mbtiData;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mbtiactivity);
 
-        recycleView = (RecyclerView)findViewById(R.id.mbti_recyclerView);
-        layoutManager = new LinearLayoutManager(this);
-
+        recycleView   = (RecyclerView)findViewById(R.id.mbti_recyclerView);
+        layoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
         adapter       = new MBTIAdapter();
+        mbtiData      = new MBTIData();
+
+        inputDataToAdapter();
 
         //여기에 이제 mbti별 특징? 클래스 만들어서 너줘야함
-        String str[][]                           = {
-                {"양승현","001-3943-583"},
+       /* String str[][]                           = {
+                {"ENTP","ENTP\n 뭐든 잘함\n 밥 좋아함\n 방학언제하지\n그렇다면 개학은?\n고민을 좀함\n"},
                 {"홍길동", "903-384-4824"},
                 { "하하","394-4279-8532"},
                 {"김범수","239-4935-2940"},
@@ -40,22 +43,26 @@ public class MBTIActivity extends AppCompatActivity {
                 {"갓 박효신", "394-2850-5847"}
         };
 
-        MbtiType mbtiFeautre[]                           = new MbtiType[str.length];
-        //for (int i = 0; i< str.length; i ++){
-          //  mbtiFeautre[i] = new MbtiType(str[i][0],str[i][1]);
-        //}
+        MbtiType mbtiFeautre[]                           = new MbtiType[str.length];*/
+
+        //초기화
+       /* for (int i = 0; i< str.length; i ++){
+            mbtiFeautre[i] = new MbtiType(str[i][0],str[i][1]);
+        }
 
         for (int i = 0; i < mbtiFeautre.length; i++){
-            adapter.addItem(new MbtiType(str[i][0],str[i][1]));
-        }
+            adapter.addItem(mbtiFeautre[i]);
+        }*/
         recycleView.setLayoutManager(layoutManager);
         recycleView.setAdapter(adapter);
 
+        adapter.notifyDataSetChanged();
 
         /**
          * bottomNavigationView함수는 bottom_navigation_menu.xml에서 정의한 메뉴 네비게이션을 통해 정의한 네비게이션 바의 객체입니다.
          * 이 객체에 이벤트 헨들러를 통해 어떤 네비게이션 바의 아이콘이 클릭 됬는지 찾는 메서드 입니다.
          */
+
         bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -81,4 +88,15 @@ public class MBTIActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * 주어진 데이터를 adapter에 추가하는 func
+     */
+    public void inputDataToAdapter(){
+        for (int i = 0; i < this.mbtiData.getMbti_type().length; i++){
+            this.adapter.addItem(this.mbtiData.getMbti_type()[i]);
+        }
+    }
 }
+
+
