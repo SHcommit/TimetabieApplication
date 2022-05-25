38e2964e4a03a1df5f4fd8f0a436598de3fab96a
Kim Seok Hyun <sam0774@naver.com>
2022-05-25 17:40:07 +0900

    Update source

--- app/src/main/java/project/timetable_recommend/SettingActivity.java
+++ app/src/main/java/project/timetable_recommend/SettingActivity.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 
 
 public class SettingActivity extends AppCompatActivity {
-
+    String MBTI;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,14 +27,13 @@ public class SettingActivity extends AppCompatActivity {
         //OK클릭 시
         dialogBuilder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int id)
-            {
+            public void onClick(DialogInterface dialog, int id) {
                 Intent intent = new Intent(getApplicationContext(), MBTIQuestionActivity.class);
                 startActivity(intent);
             }
         });
         //cancel 클릭 시
-        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+        dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 textView.setVisibility(View.VISIBLE);
@@ -43,15 +42,45 @@ public class SettingActivity extends AppCompatActivity {
             }
         });
         dialogBuilder.show();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String MBTI = editText.getText().toString();
-                if(Character.isLowerCase(MBTI.charAt(0)))
+                MBTI = editText.getText().toString();
+                if (Character.isLowerCase(MBTI.charAt(0)))
                     MBTI = MBTI.toUpperCase();
-                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                intent.putExtra("MBTI", MBTI);
-                startActivity(intent);
+                while(true) {
+                    if(
+                    MBTI == "ISFP" ||
+                    MBTI ==" ISFJ" ||
+                    MBTI == "ISTP" ||
+                    MBTI == "ISTJ" ||
+                    MBTI == "INFP" ||
+                    MBTI == "INFJ" ||
+                    MBTI == "INTP" ||
+                    MBTI == "INTJ" ||
+
+                    MBTI == "ESFP" ||
+                    MBTI ==" ESFJ" ||
+                    MBTI == "ESTP" ||
+                    MBTI == "ESTJ" ||
+                    MBTI == "ENFP" ||
+                    MBTI == "ENFJ" ||
+                    MBTI == "ENTP" ||
+                    MBTI == "ENTJ")
+                    {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        intent.putExtra("MBTI", MBTI);
+                        startActivity(intent);
+                        break;
+                    }
+
+
+
+
+
+                }
+
             }
         });
 
