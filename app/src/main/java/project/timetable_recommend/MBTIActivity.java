1b0e69f9224d66685cc88777d07300c865c2ddb5
ysh <happysh_s2@naver.com>
2022-05-27 04:19:31 +0900

    Add animated transition when click recyclerView Cell

--- app/src/main/java/project/timetable_recommend/MBTIActivity.java
+++ app/src/main/java/project/timetable_recommend/MBTIActivity.java
@@ -1,22 +1,28 @@
 package project.timetable_recommend;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
 import android.view.animation.OvershootInterpolator;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import Controller.MBTIAdapter;
 import Controller.bottomNavigationListenerInSecondActivity;
 import Model.MBTIData;
+import Model.MbtiType;
 import jp.wasabeef.recyclerview.adapters.AlphaInAnimationAdapter;
 import jp.wasabeef.recyclerview.adapters.ScaleInAnimationAdapter;
 
-public class MBTIActivity extends AppCompatActivity {
+public class MBTIActivity extends AppCompatActivity implements MBTIAdapter.OnItemClickListener{
 
     /**
      * @param bottomNavigationView : 바텀 네비게이션 바의 다음 화면을 전환하기 위해 필요한 변수
@@ -25,6 +31,9 @@ public class MBTIActivity extends AppCompatActivity {
      * @param recyclerView         : 리싸이클러 뷰를 호출하기 위해 필요한 변수
      * @param mbtiData             : 성격 유형별 특징이 저장되어있는 변수
      * @param context_mbtiActivity : 현재 mbtiActivity 화면 객체를 얻을 수 있는 변수
+     *
+     * https://developer.android.com/training/transitions/start-activity?hl=ko#java
+     * 내가 사용하려는 트렌지션 animation은 롤리팝버전이후로 지원됨...
      */
     BottomNavigationView       bottomNavigationView;
     RecyclerView.LayoutManager layoutManager;
@@ -60,7 +69,30 @@ public class MBTIActivity extends AppCompatActivity {
         alphaInAnimationAdapter.setInterpolator(new OvershootInterpolator());
         alphaInAnimationAdapter.setFirstOnly(false);
         recyclerView.setAdapter(new ScaleInAnimationAdapter(alphaInAnimationAdapter));
+        adapter.setOnItemClickListener(new MBTIAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View v, int position)  {
+                Intent intent = new Intent(MBTIActivity.this, MBTIDetailActivity.class);
+                MbtiType mbti = adapter.getItem(position);
 
+                intent.putExtra("mbtiType", mbti.getType());
+                intent.putExtra("mbtiImage",mbti.getImage());
+                View mbtiView = v.findViewById(R.id.mbti_image);
+                //View mbtiTextView = v.findViewById(R.id.mbti_type);
+                //Pair<View, String> mbti_imag = Pair.create(mbtiView, mbtiView.getTransitionName());
+
+        /*ActivityOptionsCompat option =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this, mbtiView,
+                        Pair.create(mbtiView, "mbtiImg"),
+                        Pair.create(mbtiTextView, "mbtiType"));
+           튜플 안먹힘
+           우선 튜플 안먹히니까 nbtiType transtionName빼자 추후 되는거 알면 다시 xml에 추가
+       */
+                ActivityOptionsCompat option =
+                        ActivityOptionsCompat.makeSceneTransitionAnimation(MBTIActivity.this, mbtiView, "mbtiImg");
+                startActivity(intent, option.toBundle());
+            }
+        }) ;
         /**
          * bottomNavigationView함수는 bottom_navigation_menu.xml에서 정의한 메뉴 네비게이션을 통해 정의한 네비게이션 바의 객체입니다.
          * 이 객체에 이벤트 헨들러를 통해 어떤 네비게이션 바의 아이콘이 클릭 됬는지 찾는 메서드 입니다.
@@ -78,6 +110,30 @@ public class MBTIActivity extends AppCompatActivity {
             this.adapter.addItem(this.mbtiData.getMbti_type()[i]);
         }
     }
+
+    @Override
+    public void onItemClick(View v, int position)  {
+        Intent intent = new Intent(MBTIActivity.this, MBTIDetailActivity.class);
+        MbtiType mbti = adapter.getItem(position);
+
+        intent.putExtra("mbtiType", mbti.getType());
+        intent.putExtra("mbtiImage",mbti.getImage());
+        View mbtiView = v.findViewById(R.id.mbti_image);
+        //View mbtiTextView = v.findViewById(R.id.mbti_type);
+        //Pair<View, String> mbti_imag = Pair.create(mbtiView, mbtiView.getTransitionName());
+
+        /*ActivityOptionsCompat option =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this, mbtiView,
+                        Pair.create(mbtiView, "mbtiImg"),
+                        Pair.create(mbtiTextView, "mbtiType"));
+           튜플 안먹힘
+           우선 튜플 안먹히니까 nbtiType transtionName빼자 추후 되는거 알면 다시 xml에 추가
+       */
+        ActivityOptionsCompat option =
+                ActivityOptionsCompat.makeSceneTransitionAnimation(this, mbtiView, "mbti_image_detail");
+        startActivity(intent, option.toBundle());
+    }
+
 }
 
 
