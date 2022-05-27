586ecec1f2bf98a04396d7bc5e3fae39bd1070cd
MoonDooo <moondooo@example.com>
2022-05-27 14:18:46 +0900

    Previous TimeTable add

--- app/src/main/java/project/timetable_recommend/MainActivity.java
+++ app/src/main/java/project/timetable_recommend/MainActivity.java
@@ -1,40 +1,15 @@
 package project.timetable_recommend;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 
-import android.util.Log;
-import android.view.MenuItem;
-
-import android.widget.Toast;
-
-import com.android.volley.AuthFailureError;
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.gson.Gson;
-
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.security.auth.Subject;
 
 import Model.AMPM;
 import Model.DAY;
 import Model.GsonThread;
-import Model.SubjectList;
 import Model.TableCell;
 import Controller.bottomNavigationListenerInFirstActivity;
 
---
+++ app/src/main/java/project/timetable_recommend/PreviousLayout2.java
@@ -0,0 +1,28 @@
+package project.timetable_recommend;
+
+import static Model.GsonThread.subjectList;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import Controller.PreviousAdapter;
+
+public class PreviousLayout2 extends AppCompatActivity {
+    PreviousAdapter adapter;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_previous_layout2);
+        recyclerView = (RecyclerView)findViewById(R.id.recycler);
+        layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        adapter = new PreviousAdapter();
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
+    }
+}
