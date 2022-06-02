50d5b6c31f3ffa912b73d25c3c7e433cc10f14b7
ysh <happysh_s2@naver.com>
2022-06-02 16:35:00 +0900

    aa

--- app/src/main/java/project/timetable_recommend/activity/controller/GsonThread.java
+++ app/src/main/java/project/timetable_recommend/activity/controller/GsonThread.java
@@ -34,7 +34,7 @@ public class GsonThread extends Thread{
         makeRequest();
     }
     public void makeRequest() {
-        String url            = "http://192.168.0.109:80/test/Android/android.jsp";
+        String url            = "http://192.168.0.112:80/test/Android/android.jsp";
         StringRequest request = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
